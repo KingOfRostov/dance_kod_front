bde693224f2e50af15f6d0b5c97b3f3a127ef4c3
KingOfRostov <mk-serg@yandex.ru>
2023-08-15 12:39:31 +0300

    Introduction and Garanties components

---
+++ src/components/introduction/introduction.tsx
@@ -0,0 +1,15 @@
+
+export const Introduction = () => {
+  return (
+    <div>
+      <h3 className="text-center">
+        Пробная тренировка - <span className="red">Бесплатно</span>!
+      </h3>
+      <p className="text-center">
+        Место, где <span className="red">каждый</span> ребенок или
+        подросток сможет получить <span className="red">качественное</span>
+        обучение танцевальному искусству
+      </p>
+    </div>
+  );
+};
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -6,6 +6,8 @@ import { InvitingForm } from '~/components/inviting_form/inviting_form'
 import { MainDirections } from '~/components/main_directions/main_directions'
 import { KidsCarousel } from '~/components/kids_carousel/kids_carousel'
 import { PhoneButton } from '~/components/phone_button/phone_button'
+import { Garanties } from '~/components/garanties/garanties'
+import { Introduction } from '~/components/introduction/introduction'
 
 export default function Home() {
   const TatianaPhoneNumber: string = '+7 (918) 571 26 89';
@@ -15,39 +17,12 @@ export default function Home() {
 
   return (
     <Layout>
-      <h3 className="text-center">
-        Пробная тренировка - <span className="red">Бесплатно</span>!
-      </h3>
-      <p className="text-center">
-        Место, где <span className="red">каждый</span> ребенок или
-        подросток сможет получить <span className="red">качественное</span>
-        обучение танцевальному искусству
-      </p>
+
+      <Introduction />
 
       <hr />
 
-      <h1 className="page-header-text">Почему</h1>
-      <h1 className="page-header-text">
-        <span className="red">‌Вам нужно в KOD</span>?
-      </h1>
-
-      <p className="text-justify">
-        Мы <span className="red">гарантируем</span> профессиональный и
-        грамотный подход к <span className="red">каждому </span>
-        ученику и <span className="red">рост</span> в танцевальных
-        направлениях
-        <br />
-        <br />
-        Нас ждут множество мероприятий, конкурсов, фестивалей, мастер-классов и
-        насыщенных занятий
-        <br />
-        <br />В нашей <span className="red">команде</span> есть место тем,
-        кто разделяет нашу <span className="red">любовь</span> к танцам и
-        готов отдаться этому <span className="red">искусству</span>
-        <br />
-        <br />С <span className="red font-caveat fs-20">KOD</span> путь к
-        большой сцене будет насыщенным и очень интересным
-      </p>
+      <Garanties />
 
       <div>
         <h1 className="page-header-text">
